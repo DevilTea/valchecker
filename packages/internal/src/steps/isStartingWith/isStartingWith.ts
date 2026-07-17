998a94adc0d8db13dab608eabf22159fd923fbcd
DevilTea <ch19980814@gmail.com>
2026-07-17 12:12:07 +0800

    refactor: align isStartingWith module path

---
+++ packages/internal/src/steps/isStartingWith/isStartingWith.ts
@@ -0,0 +1,59 @@
+import type { DefineExpectedValchecker, DefineStepMethod, DefineStepMethodMeta, ExecutionIssue, MessageHandler, Next, TStepPluginDef } from '../../core'
+import { implStepPlugin } from '../../core'
+
+type Meta = DefineStepMethodMeta<{
+	Name: 'isStartingWith'
+	ExpectedCurrentValchecker: DefineExpectedValchecker<{ output: string }>
+	SelfIssue: ExecutionIssue<'isStartingWith:expected_starting_with', { value: string, prefix: string }>
+}>
+
+interface PluginDef extends TStepPluginDef {
+	/**
+	 * ### Description:
+	 * Checks that the string starts with the specified prefix.
+	 *
+	 * ---
+	 *
+	 * ### Example:
+	 * ```ts
+	 * import { createValchecker, isStartingWith, string } from 'valchecker'
+	 *
+	 * const v = createValchecker({ steps: [string, isStartingWith] })
+	 * const schema = v.string().isStartingWith('hello')
+	 * const result = schema.execute('hello world')
+	 * ```
+	 *
+	 * ---
+	 *
+	 * ### Issues:
+	 * - `'isStartingWith:expected_starting_with'`: The string does not start with the specified prefix.
+	 */
+	isStartingWith: DefineStepMethod<
+		Meta,
+		this['CurrentValchecker'] extends Meta['ExpectedCurrentValchecker']
+			? (prefix: string, message?: MessageHandler<Meta['SelfIssue']>) => Next<
+					{ issue: Meta['SelfIssue'] },
+					this['CurrentValchecker']
+				>
+			: never
+	>
+}
+
+/* @__NO_SIDE_EFFECTS__ */
+export const isStartingWith = implStepPlugin<PluginDef>({
+	isStartingWith: ({
+		utils: { addSuccessStep, success, createIssue, failure },
+		params: [prefix, message],
+	}) => {
+		addSuccessStep(value => value.startsWith(prefix)
+			? success(value)
+			: failure(
+					createIssue({
+						code: 'isStartingWith:expected_starting_with',
+						payload: { value, prefix },
+						customMessage: message,
+						defaultMessage: `Expected the string to start with "${prefix}".`,
+					}),
+				))
+	},
+})
