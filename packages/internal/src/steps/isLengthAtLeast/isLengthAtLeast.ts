5008ef4a666974ae0f4f460faa41ea2b47aedd1d
DevilTea <ch19980814@gmail.com>
2026-07-17 12:29:08 +0800

    refactor: preserve original isLengthAtLeast implementation symbols

--- packages/internal/src/steps/isLengthAtLeast/isLengthAtLeast.ts
+++ packages/internal/src/steps/isLengthAtLeast/isLengthAtLeast.ts
@@ -1,20 +1,20 @@
 import type { DefineExpectedValchecker, DefineStepMethod, DefineStepMethodMeta, ExecutionIssue, InferOutput, MessageHandler, Next, TStepPluginDef } from '../../core'
 import { implStepPlugin } from '../../core'
 
-declare namespace Internal {
+declare namespace LengthAtLeastInternal {
 	export type Issue<T extends { length: number } = { length: number }> = ExecutionIssue<
 		'isLengthAtLeast:expected_length_at_least',
 		{ value: T, minimum: number }
 	>
 }
 
-type Meta = DefineStepMethodMeta<{
+type LengthAtLeastMeta = DefineStepMethodMeta<{
 	Name: 'isLengthAtLeast'
 	ExpectedCurrentValchecker: DefineExpectedValchecker<{ output: { length: number } }>
-	SelfIssue: Internal.Issue
+	SelfIssue: LengthAtLeastInternal.Issue
 }>
 
-interface PluginDef extends TStepPluginDef {
+interface LengthAtLeastPluginDef extends TStepPluginDef {
 	/**
 	 * ### Description:
 	 * Checks that the value's length is greater than or equal to the specified minimum.
@@ -36,11 +36,11 @@ interface PluginDef extends TStepPluginDef {
 	 * - `'isLengthAtLeast:expected_length_at_least'`: The value is shorter than the minimum length.
 	 */
 	isLengthAtLeast: DefineStepMethod<
-		Meta,
-		this['CurrentValchecker'] extends Meta['ExpectedCurrentValchecker']
+		LengthAtLeastMeta,
+		this['CurrentValchecker'] extends LengthAtLeastMeta['ExpectedCurrentValchecker']
 			? InferOutput<this['CurrentValchecker']> extends infer CurrentOutput extends { length: number }
-				? (minimum: number, message?: MessageHandler<Internal.Issue<CurrentOutput>>) => Next<
-						{ issue: Internal.Issue<CurrentOutput> },
+				? (minimum: number, message?: MessageHandler<LengthAtLeastInternal.Issue<CurrentOutput>>) => Next<
+						{ issue: LengthAtLeastInternal.Issue<CurrentOutput> },
 						this['CurrentValchecker']
 					>
 				: never
@@ -49,7 +49,7 @@ interface PluginDef extends TStepPluginDef {
 }
 
 /* @__NO_SIDE_EFFECTS__ */
-export const isLengthAtLeast = implStepPlugin<PluginDef>({
+export const isLengthAtLeast = implStepPlugin<LengthAtLeastPluginDef>({
 	isLengthAtLeast: ({
 		utils: { addSuccessStep, success, createIssue, failure },
 		params: [minimum, message],
