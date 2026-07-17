d072ee82058ad39aadb5aad30558e14b8a12f4e1
DevilTea <ch19980814@gmail.com>
2026-07-17 12:24:54 +0800

    refactor: split isLengthAtMost benchmarks

---
+++ packages/internal/src/steps/isLengthAtMost/isLengthAtMost.bench.ts
@@ -0,0 +1,14 @@
+import { bench, describe } from 'vitest'
+import { createValchecker, isLengthAtMost, string } from '../..'
+
+const schema = createValchecker({ steps: [string, isLengthAtMost] }).string().isLengthAtMost(10)
+
+describe('isLengthAtMost benchmarks', () => {
+	bench('length success', () => {
+		schema.execute('value')
+	})
+
+	bench('length failure', () => {
+		schema.execute('value that is too long')
+	})
+})
