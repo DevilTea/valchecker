e015f9a4604fea5ce1ee292810214fd2c93836c1
DevilTea <ch19980814@gmail.com>
2026-07-17 12:19:42 +0800

    refactor: align toSplit module path

---
+++ packages/internal/src/steps/toSplit/toSplit.ts
@@ -0,0 +1,52 @@
+import type { DefineExpectedValchecker, DefineStepMethod, DefineStepMethodMeta, InferOutput, Next, TStepPluginDef } from '../../core'
+import type { OverloadParametersAndReturnType } from '../../shared'
+import { implStepPlugin } from '../../core'
+
+type Meta = DefineStepMethodMeta<{
+	Name: 'toSplit'
+	ExpectedCurrentValchecker: DefineExpectedValchecker<{ output: string & { split: (...params: any[]) => string[] } }>
+}>
+
+interface PluginDef extends TStepPluginDef {
+	/**
+	 * ### Description:
+	 * Splits the string using the provided separator.
+	 *
+	 * ---
+	 *
+	 * ### Example:
+	 * ```ts
+	 * import { createValchecker, string, toSplit } from 'valchecker'
+	 *
+	 * const v = createValchecker({ steps: [string, toSplit] })
+	 * const schema = v.string().toSplit(',')
+	 * const result = schema.execute('a,b,c')
+	 * // result.value: ['a', 'b', 'c']
+	 * ```
+	 *
+	 * ---
+	 *
+	 * ### Issues:
+	 * None.
+	 */
+	toSplit: this['CurrentValchecker'] extends Meta['ExpectedCurrentValchecker']
+		? OverloadParametersAndReturnType<InferOutput<this['CurrentValchecker']>['split']> extends infer Tuple
+			? Tuple extends [params: any[], ret: any]
+				? DefineStepMethod<
+					Meta,
+					(...params: Tuple[0]) => Next<{ output: Tuple[1] }, this['CurrentValchecker']>
+				>
+				: never
+			: never
+		: never
+}
+
+/* @__NO_SIDE_EFFECTS__ */
+export const toSplit = implStepPlugin<PluginDef>({
+	toSplit: ({
+		utils: { addSuccessStep, success },
+		params,
+	}) => {
+		addSuccessStep(value => success(value.split(...params)))
+	},
+})
