fff040753a0798ee079ea6e589c6dc2f307aad0e
DevilTea <ch19980814@gmail.com>
2026-07-19 01:31:37 +0800

    bench: add Map value membership cases

---
+++ packages/internal/src/steps/isIncludingValue/isIncludingValue.bench.ts
@@ -0,0 +1,12 @@
+import { bench, describe } from 'vitest'
+import { createValchecker, isIncludingValue, map, number, string } from '../..'
+
+const v = createValchecker({ steps: [isIncludingValue, map, number, string] })
+const schema = v.map({ key: v.string(), value: v.number() }).isIncludingValue(1)
+const hit = new Map([['target', 1]])
+const miss = new Map([['other', 2]])
+
+describe('isIncludingValue benchmarks', () => {
+	bench('hit', () => schema.execute(hit))
+	bench('miss', () => schema.execute(miss))
+})
