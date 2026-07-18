c6ed50146d89a71d1d8a393a0db276ba5b5e90cf
DevilTea <ch19980814@gmail.com>
2026-07-19 02:07:43 +0800

    feat: add Map key array transform

---
+++ packages/internal/src/steps/toKeys/toKeys.ts
@@ -0,0 +1,22 @@
+import type { DefineExpectedValchecker, DefineStepMethod, DefineStepMethodMeta, InferOutput, Next, TStepPluginDef } from '../../core'
+import { implStepPlugin } from '../../core'
+
+type Meta = DefineStepMethodMeta<{
+	Name: 'toKeys'
+	ExpectedCurrentValchecker: DefineExpectedValchecker<{ output: Map<any, any> }>
+}>
+
+interface PluginDef extends TStepPluginDef {
+	toKeys: DefineStepMethod<Meta, this['CurrentValchecker'] extends infer This extends Meta['ExpectedCurrentValchecker']
+		? InferOutput<This> extends Map<infer Key, any>
+			? () => Next<{ output: Key[] }, This>
+			: never
+		: never>
+}
+
+/* @__NO_SIDE_EFFECTS__ */
+export const toKeys = implStepPlugin<PluginDef>({
+	toKeys: ({ utils: { addSuccessStep, success } }) => {
+		addSuccessStep(value => success([...value.keys()]))
+	},
+})
