e6a0e4dbddf17f1772efe473214b0bad74213da2
DevilTea <ch19980814@gmail.com>
2026-07-19 09:16:16 +0800

    feat: extend toMapped to Set values

--- packages/internal/src/steps/toMapped/toMapped.ts
+++ packages/internal/src/steps/toMapped/toMapped.ts
@@ -2,34 +2,72 @@ import type { DefineExpectedValchecker, DefineStepMethod, DefineStepMethodMeta, 
 import { implStepPlugin } from '../../core'
 
 declare namespace Internal {
-	export type Issue<Input extends any[] = any[], Item = Input[number]> = ExecutionIssue<
+	export type CallbackIssue<Input = any, Item = any> = ExecutionIssue<
 		'toMapped:callback_failed',
 		{ value: Input, item: Item, index: number, error: unknown },
 		'operation'
 	>
-	export interface Options<Input extends any[] = any[]> extends StepOptions<Issue<Input>> {
+	export type DuplicateMappedItemIssue<Input extends Set<any> = Set<any>, Mapped = unknown> = ExecutionIssue<
+		'toMapped:duplicate_mapped_item',
+		{
+			value: Input
+			firstItem: Input extends Set<infer Item> ? Item : never
+			item: Input extends Set<infer Item> ? Item : never
+			mappedItem: Mapped
+			firstIndex: number
+			index: number
+		}
+	>
+	export interface Options<Issue extends ExecutionIssue = ExecutionIssue> extends StepOptions<Issue> {
 		readonly thisArg?: any
 	}
 }
 
 type Meta = DefineStepMethodMeta<{
 	Name: 'toMapped'
-	ExpectedCurrentValchecker: DefineExpectedValchecker<{ output: any[] & { map: (...params: any[]) => any } }>
-	SelfIssue: Internal.Issue
+	ExpectedCurrentValchecker: DefineExpectedValchecker<{ output: any[] | Set<any> }>
+	SelfIssue: Internal.CallbackIssue | Internal.DuplicateMappedItemIssue
 }>
 
 interface PluginDef extends TStepPluginDef {
-	toMapped: DefineStepMethod<
-		Meta,
-		this['CurrentValchecker'] extends infer This extends Meta['ExpectedCurrentValchecker']
-			? InferOutput<This> extends infer Input extends any[]
-				? <Mapped>(
-						mapper: (item: Input[number], index: number, value: Input) => Mapped,
-						options?: Internal.Options<Input>,
-					) => Next<{ output: Mapped[], issue: Internal.Issue<Input> }, This>
+	toMapped:
+		| DefineStepMethod<
+			Meta,
+			this['CurrentValchecker'] extends infer This extends Meta['ExpectedCurrentValchecker']
+				? InferOutput<This> extends infer Input extends any[]
+					? <Mapped>(
+							mapper: (item: Input[number], index: number, value: Input) => Mapped,
+							options?: Internal.Options<Internal.CallbackIssue<Input, Input[number]>>,
+						) => Next<{
+							output: Mapped[]
+							issue: Internal.CallbackIssue<Input, Input[number]>
+						}, This>
+					: never
 				: never
-			: never
-	>
+		>
+		| DefineStepMethod<
+			Meta,
+			this['CurrentValchecker'] extends infer This extends Meta['ExpectedCurrentValchecker']
+				? InferOutput<This> extends infer Input extends Set<any>
+					? <Mapped>(
+							mapper: (
+								item: Input extends Set<infer Item> ? Item : never,
+								index: number,
+								value: Input,
+							) => Mapped,
+							options?: Internal.Options<
+								| Internal.CallbackIssue<Input, Input extends Set<infer Item> ? Item : never>
+								| Internal.DuplicateMappedItemIssue<Input, Mapped>
+							>,
+						) => Next<{
+							output: Set<Mapped>
+							issue:
+								| Internal.CallbackIssue<Input, Input extends Set<infer Item> ? Item : never>
+								| Internal.DuplicateMappedItemIssue<Input, Mapped>
+						}, This>
+					: never
+				: never
+		>
 }
 
 class MapCallbackError {
@@ -47,27 +85,72 @@ export const toMapped = implStepPlugin<PluginDef>({
 		params: [mapper, options],
 	}) => {
 		addSuccessStep((value) => {
-			try {
-				return success(value.map((item: unknown, index: number, array: unknown[]) => {
-					try {
-						return mapper.call(options?.thisArg, item, index, array as any)
-					}
-					catch (error) {
-						throw new MapCallbackError(item, index, error)
-					}
-				}))
+			if (Array.isArray(value)) {
+				try {
+					return success(value.map((item: unknown, index: number, array: unknown[]) => {
+						try {
+							return mapper.call(options?.thisArg, item, index, array)
+						}
+						catch (error) {
+							throw new MapCallbackError(item, index, error)
+						}
+					}))
+				}
+				catch (error) {
+					if (!(error instanceof MapCallbackError))
+						throw error
+					return failure(createIssue({
+						code: 'toMapped:callback_failed',
+						category: 'operation',
+						payload: { value, item: error.item, index: error.index, error: error.error },
+						customMessage: options?.message,
+						defaultMessage: 'Map callback failed.',
+					}))
+				}
 			}
-			catch (error) {
-				if (!(error instanceof MapCallbackError))
-					throw error
-				return failure(createIssue({
-					code: 'toMapped:callback_failed',
-					category: 'operation',
-					payload: { value, item: error.item, index: error.index, error: error.error },
-					customMessage: options?.message,
-					defaultMessage: 'Map callback failed.',
-				}))
+
+			const output = new Set<unknown>()
+			const firstItems = new Map<unknown, { item: unknown, index: number }>()
+			let index = 0
+			for (const item of value) {
+				let mappedItem: unknown
+				try {
+					mappedItem = mapper.call(options?.thisArg, item, index, value)
+				}
+				catch (error) {
+					return failure(createIssue({
+						code: 'toMapped:callback_failed',
+						category: 'operation',
+						payload: { value, item, index, error },
+						customMessage: options?.message,
+						defaultMessage: 'Map callback failed.',
+					}))
+				}
+
+				if (output.has(mappedItem)) {
+					const first = firstItems.get(mappedItem)
+					if (first == null)
+						throw new Error('Missing mapped Set item metadata.')
+					return failure(createIssue({
+						code: 'toMapped:duplicate_mapped_item',
+						payload: {
+							value,
+							firstItem: first.item,
+							item,
+							mappedItem,
+							firstIndex: first.index,
+							index,
+						},
+						customMessage: options?.message,
+						defaultMessage: 'Expected mapped Set items to be unique.',
+					}))
+				}
+
+				output.add(mappedItem)
+				firstItems.set(mappedItem, { item, index })
+				index++
 			}
+			return success(output)
 		})
 	},
 })
